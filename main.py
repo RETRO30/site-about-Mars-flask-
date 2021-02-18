58b7b0503988b257dfbe9bcffdedbb1660cfc881
Stanislav <59125462+RETRO30@users.noreply.github.com>
2021-02-18 23:31:03 +0500

    Реклама с картинкой

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from flask import url_for
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def start_page():
     return "Миссия Колонизация Марса"
@@ -33,6 +32,32 @@ def image_mars():
     <p><img src="{url_for('static', filename='img/mars_image_1.png')}" alt="здесь должна была быть картинка, но не нашлась">
     <br>Вот она какая, красная планета.</br></p></body>
     </html>"""
+    return data
+
+
+@app.route('/promotion_image')
+def promotion_image():
+    data = f"""<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Привет, Марс!</title>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css">
+        <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
+    </head>
+    <body>
+        <h1>Жди нас, Марс!</h1>
+            <p>
+                <img src="{url_for('static', filename='img/mars_image_1.png')}" alt="здесь должна была быть картинка, но не нашлась">
+                <div class="alert alert-dark" role="alert"><strong>Человечество вырастает из детства.</strong></div>
+                <div class="alert alert-success" role="alert"><strong>Человечеству мала одна планета.</strong></div>
+                <div class="alert alert-secondary" role="alert"><strong>Мы сделаем обитаемыми безжизненные пока планеты.</strong></div>
+                <div class="alert alert-warning" role="alert"><strong>И начнем с Марса!</strong></div>
+                <div class="alert alert-danger" role="alert"><strong>Присоединяйся!</strong></div>
+            </p>
+    </body>
+</html>
+"""
     return data
 
 
